381880eba0693d758223f17cada0c55833800844
lvirgini <lvirgini@student.42.fr>
2020-03-04 22:36:23 +0100

    printf pb non fix

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: lvirgini <lvirgini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/29 13:26:47 by lvirgini          #+#    #+#             */
-/*   Updated: 2020/03/04 15:30:08 by lvirgini         ###   ########.fr       */
+/*   Updated: 2020/03/04 21:12:47 by lvirgini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,15 @@ t_app		*g_app;
 
 int		main(int ac, char **av)
 {
+	
+	
+	printf("wtf\n");
+	ft_printf("wtf2\n");
+	/*
 	g_app = malloc_application();
 	if (file_checking(ac, av) == -1)
 		return (-1);
 	free_application(g_app);
+	*/
 	return (0);
 }
